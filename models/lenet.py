5d7f6507e045bee7eeef27132a59f76fdb7d505e
Ryan Cheung <zhangxiaoya1989@gmail.com>
2021-06-24 11:24:38 +0800

    [Update] lenet file

--- models/lenet.py
+++ models/lenet.py
@@ -1,1 +1,24 @@
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 6, kernel_size = 5)
+        self.conv2 = nn.Conv2d(in_channels = 6, out_channels = 16, kernel_size= 5)
+        self.fc1 = nn.Linear(in_features=16 * 5 * 5, out_features= 120)
+        self.fc2 = nn.Linear(in_features=120, out_features=84)
+        self.fc3 = nn.Linear(in_features=84, out_features=10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = torch.flatten(out, 1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
